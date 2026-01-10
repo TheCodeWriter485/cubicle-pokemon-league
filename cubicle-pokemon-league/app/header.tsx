7d2ce18a6338385ef1ac83aed5046404e0a3ac82
TJ Vitchutripop <tjvitchutripop@TJs-MacBook-Pro-9.local>
2026-01-10 17:24:19 -0500

    Finished navbar + routing

---
+++ cubicle-pokemon-league/app/header.tsx
@@ -0,0 +1,18 @@
+import Navbar from "./navbar";
+import Image from "next/image";
+
+export default function Header(){
+    return (
+        <div>
+        <Image
+          src="/Cubical.png"
+          alt="Cubical Logo"
+          width={200}
+          height={20}
+          style={{margin:"0 auto", marginTop: "2rem"}}
+          priority
+        />
+      <Navbar />
+        </div>
+    );
+}
