73a8eeb30439b47fbe84fc99e2fe1a61e7d4cf6e
ultrasuperbeast <thecodewriter485@gmail.com>
2026-04-04 09:36:02 -0400

    minor push
    Just comments for page.tsx for tierlist

--- cpl/app/tier-list/page.tsx
+++ cpl/app/tier-list/page.tsx
@@ -43,13 +43,30 @@ export default function TierList()
         <h1>
           Tier List
         </h1>
+<<<<<<< Updated upstream
         <div style={ { display: "flex" } }>
+=======
+        {/* Beginning of card
+        Flex allows grid? */}
+        <div style={{ display: "flex"}}>
+          {/* map through pokemon database */}
+>>>>>>> Stashed changes
           { pokemon.map((tier, tierIndex) => (
+            // tier is the point from the database
+            // tier index is the value you use to group pokemon
             <div key={ tierIndex }>
               { tier.map((poke: any, pokeIndex: number) => (
+                // pokemon card class
                 <div key={ pokeIndex } className="pokemon-card">
+<<<<<<< Updated upstream
                   <script>console.log(pokeIndex)</script>
                   <Card name={ poke.NamePoke } value={ poke.PointValue } image={ poke.ID } />
+=======
+                {/* Using poke as the object, Card takes a string for name
+                takes a number for value
+                and ID for image */}
+                  <Card name={ poke.NamePoke } value={ poke.PointValue } image={poke.ID} />
+>>>>>>> Stashed changes
                 </div>
               )) }
             </div>
